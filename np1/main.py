6a5b2522dfa9af0f394f3823ce983ba6a160d05d
XZDXRZ <13918165843@163.com>
2021-02-19 20:52:29 +0800

    Add backgroundImg

--- np1/main.py
+++ np1/main.py
@@ -2,13 +2,16 @@ import pygame, os
 
 pygame.init()
 
-gamescreen = pygame.display.set_mode([640, 480])
+gamescreen = pygame.display.set_mode([756, 535])
 running = True
+pygame.display.set_caption("np1")
+bg_img = pygame.image.load("./img/bg_img.jpg")
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    gamescreen.blit(bg_img,[0,0])
 
     pygame.display.flip()
 
