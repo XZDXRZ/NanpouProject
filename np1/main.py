10f94693a40a3ecee061ccfc3f036ccdf715e119
XZDXRZ <13918165843@163.com>
2021-02-19 21:07:16 +0800

    Add some const(?)

--- np1/main.py
+++ np1/main.py
@@ -1,8 +1,22 @@
 import pygame, os
 
 pygame.init()
+size = (756, 535)
+tick = 10
 
-gamescreen = pygame.display.set_mode([756, 535])
+MAXENERMY = 3
+MAXENERMYSPEED = 10
+MAXPLAYERBULLET = 100
+PLAYERBULLETDELAY = 100
+MAXENERMYBULLET = 7
+ENERMYBULLETDELAY = 500
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        img = pygame.imame.load("./img/player/plane_1.png")
+
+
+gamescreen = pygame.display.set_mode(size)
 running = True
 pygame.display.set_caption("np1")
 bg_img = pygame.image.load("./img/bg_img.jpg")
