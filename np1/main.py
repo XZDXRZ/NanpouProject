928e840094d34605fccfe68480a9c01cda3a5c03
XZDXRZ <13918165843@163.com>
2021-02-20 19:59:34 +0800

    Add start image

--- np1/main.py
+++ np1/main.py
@@ -13,19 +13,39 @@ ENERMYBULLETDELAY = 500
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        img = pygame.imame.load("./img/player/plane_1.png")
+        pygame.sprite.Sprite.__init__(self)
+        self.img = pygame.imame.load("./img/player/plane_1.png")
+        self.rect = self.img.get_rect()
+        self.rect.left, self.rect.top = (size[0]/2, size[1]/5*4)
+        self.mask = pygame.mask.from_surface(self.img)
+    
+    #def move(self):
 
-
-gamescreen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size)
 running = True
 pygame.display.set_caption("np1")
 bg_img = pygame.image.load("./img/bg_img.jpg")
+st_img = pygame.image.load("./img/st_img.jpg")
+press_any_key_to_start = pygame.font.SysFont('Tiems', 40).render("Press any key to start...", True, (0,100,0))
 
+# Start Image
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        elif event.type == pygame.KEYDOWN:
+            running = False
+    screen.blit(st_img,[0, 0])
+    screen.blit(press_any_key_to_start, (size[0]/4, size[1]/2))
+    pygame.display.flip()
+
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    gamescreen.blit(bg_img,[0,0])
+    screen.blit(bg_img,[0,0])
 
     pygame.display.flip()
 
